70b03b7153aa40b4f0e6def0a9fccd44b25570d0
Gilia Gongora <giliagongora.g@gmail.com>
2017-05-27 19:31:47 -0400

    Solucionando detalles

--- asset/js/javascript.js
+++ asset/js/javascript.js
@@ -1,1 +1,35 @@
-ajsdhksaj
+/* Verificando que el enlace funciona mediante un alert */
+alert("¡Hello World!")
+
+
+
+
+function frutas(){
+
+	var elementos = ["Manzana","Pera","Piña","Cereza","Naranja"];
+
+/* LLamdo el input de texto y lo inserto en la variable a */
+	var a =document.getElementById("textoIngresado").value;
+/* Creo el elemento li de la lista y la inserto en variable b,
+con createElement para que se imprima en el navegador */
+	var b = document.createElement("compra");
+
+/*
+	if(a.value !== elementos.length ){
+		alert("Esa fruta no está en catálogo");
+	}
+*/
+
+
+
+/* Mi variable a la insert en la variable b mediante inner que genera la impresion del
+elemento escrito en el input en el navegador */
+	b.innerHTML = a + "<br>";
+/* En catalogo de frutas inserto la variable b, es decir, b sera hijo de compras */
+	document.getElementById("compra").appendChild(b);
+/* Finalmente al texto una vez ingresado le inserto vacio despues que se le hace click */
+	document.getElementById("textoIngresado").value =  "";
+
+
+}
+
